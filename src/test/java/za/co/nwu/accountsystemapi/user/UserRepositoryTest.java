34e4b64a0963416bcd3c01edbe4ad2ed79821008
Lloyd Janse van Rensburg <thefullstackjunkie@gmail.com>
2021-10-07 21:21:38 +0200

    added first unit tests

--- src/test/java/za/co/nwu/accountsystemapi/user/UserRepositoryTest.java
+++ src/test/java/za/co/nwu/accountsystemapi/user/UserRepositoryTest.java
@@ -1,19 +1,30 @@
 package za.co.nwu.accountsystemapi.user;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.util.Optional;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@DataJpaTest
 public class UserRepositoryTest {
 
     @Autowired
     private  UserRepository underTest;
 
+    @AfterEach
+    void tearDown() {
+        underTest.deleteAll();
+    }
+
     @Test
     void findUserByEmail() {
         // given
         String email = "lloydjvrensburg@gmail.com";
+
 
         User user = new User(
                 "Lloyd",
@@ -27,6 +38,6 @@ public class UserRepositoryTest {
         Optional<User> exists = underTest.findUserByEmail(email);
 
         // then
-
+        assertThat(exists).isNotNull();
     }
 }
