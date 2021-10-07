3bae7ed30519129f085161bf77ecdecc45a71958
Lloyd Janse van Rensburg <thefullstackjunkie@gmail.com>
2021-10-07 14:39:52 +0200

    Basic Swagger Setup

--- src/main/java/za/co/nwu/accountsystemapi/AccountSystemApiApplication.java
+++ src/main/java/za/co/nwu/accountsystemapi/AccountSystemApiApplication.java
@@ -2,8 +2,10 @@ package za.co.nwu.accountsystemapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class AccountSystemApiApplication {
 
     public static void main(String[] args)
