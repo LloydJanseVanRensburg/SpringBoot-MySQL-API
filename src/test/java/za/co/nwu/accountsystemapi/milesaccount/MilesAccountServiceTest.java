03f9e8633ac31b14250be6cde847ea5c270339a2
Lloyd Janse van Rensburg <thefullstackjunkie@gmail.com>
2021-10-09 15:39:34 +0200

    Add starting point for MilesAccount unit tests

---
+++ src/test/java/za/co/nwu/accountsystemapi/milesaccount/MilesAccountServiceTest.java
@@ -0,0 +1,37 @@
+package za.co.nwu.accountsystemapi.milesaccount;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MilesAccountServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getMilesAccounts() {
+    }
+
+    @Test
+    void getMilesAccount() {
+    }
+
+    @Test
+    void createMilesAccount() {
+    }
+
+    @Test
+    void addMilesToAccount() {
+    }
+
+    @Test
+    void removeMilesFromAccount() {
+    }
+
+    @Test
+    void deleteMilesAccount() {
+    }
+}
