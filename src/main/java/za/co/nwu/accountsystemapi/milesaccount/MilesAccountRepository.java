a95eda365b96cc17bb75f008f68821a08856867f
Lloyd Janse van Rensburg <thefullstackjunkie@gmail.com>
2021-10-09 21:31:16 +0200

    MilesAccount Services Mock Unit tests almost done

--- src/main/java/za/co/nwu/accountsystemapi/milesaccount/MilesAccountRepository.java
+++ src/main/java/za/co/nwu/accountsystemapi/milesaccount/MilesAccountRepository.java
@@ -1,7 +1,16 @@
 package za.co.nwu.accountsystemapi.milesaccount;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface MilesAccountRepository extends JpaRepository<MilesAccount, Integer> {
+
+    @Query("SELECT a FROM MilesAccount a WHERE a.userId = :userId")
+    Optional<MilesAccount> findMilesAccountByUserId(@Param("userId") int userId);
 
 }
--- src/test/java/za/co/nwu/accountsystemapi/milesaccount/MilesAccountServiceTest.java
+++ src/test/java/za/co/nwu/accountsystemapi/milesaccount/MilesAccountServiceTest.java
@@ -3,8 +3,20 @@ package za.co.nwu.accountsystemapi.milesaccount;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import za.co.nwu.accountsystemapi.exception.ApiRequestException;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 class MilesAccountServiceTest {
@@ -16,20 +28,65 @@ class MilesAccountServiceTest {
 
     @BeforeEach
     void setUp() {
-        existingMilesAccount = new MilesAccount();
+        existingMilesAccount = new MilesAccount(1, 1, 100);
         milesAccountServiceUnderTest = new MilesAccountService(milesAccountRepository);
     }
 
     @Test
-    void getMilesAccounts() {
+    void getMilesAccountsTest() {
+        // when
+        milesAccountServiceUnderTest.getMilesAccounts();
+
+        // then
+        verify(milesAccountRepository).findAll();
     }
 
     @Test
-    void getMilesAccount() {
+    void canMilesAccountTest() {
+        Mockito.when(milesAccountRepository.findById(existingMilesAccount.getId())).thenReturn(Optional.of(existingMilesAccount));
+        MilesAccount foundMilesAccount = milesAccountServiceUnderTest.getMilesAccount(existingMilesAccount.getId());
+        assertThat(foundMilesAccount.getId()).isEqualTo(foundMilesAccount.getId());
     }
 
     @Test
-    void createMilesAccount() {
+    void canNotMilesAccountTest() {
+        Mockito.when(milesAccountRepository.findById(existingMilesAccount.getId())).thenReturn(Optional.empty());
+        assertThrows(ApiRequestException.class, () ->
+                milesAccountServiceUnderTest.getMilesAccount(existingMilesAccount.getId())
+        );
+    }
+
+    @Test
+    void canCreateMilesAccount() {
+        //given
+        MilesAccount newMilesAccount = new MilesAccount(1, 100);
+
+        //when
+        milesAccountServiceUnderTest.createMilesAccount(newMilesAccount);
+
+        //then
+        ArgumentCaptor<MilesAccount> milesAccountArgumentCaptor = ArgumentCaptor.forClass(MilesAccount.class);
+
+        verify(milesAccountRepository).save(milesAccountArgumentCaptor.capture());
+
+        MilesAccount capturedAccount = milesAccountArgumentCaptor.getValue();
+
+        assertThat(capturedAccount).isEqualTo(newMilesAccount);
+    }
+
+    @Test
+    void canNotCreateMilesAccount() {
+        // given
+        MilesAccount newMilesAccount = new MilesAccount(1, 100);
+
+        given(milesAccountRepository.findMilesAccountByUserId(newMilesAccount.getUserId()))
+                .willReturn(Optional.of(newMilesAccount));
+
+        // when
+        // then
+        assertThatThrownBy(() -> milesAccountServiceUnderTest.createMilesAccount(newMilesAccount))
+                .isInstanceOf(ApiRequestException.class)
+                .hasMessageContaining("This user already has an MilesAccount");
     }
 
     @Test
@@ -42,5 +99,7 @@ class MilesAccountServiceTest {
 
     @Test
     void deleteMilesAccount() {
+        Mockito.when(milesAccountRepository.findById(existingMilesAccount.getId())).thenReturn(Optional.of(existingMilesAccount));
+        assertDoesNotThrow(()-> milesAccountServiceUnderTest.deleteMilesAccount(existingMilesAccount.getId()));
     }
 }
